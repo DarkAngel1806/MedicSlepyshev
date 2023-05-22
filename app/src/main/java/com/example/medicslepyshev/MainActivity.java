af065b42d37a23924e90b486dee6f7fddc3a47af
rmp <|"/>
2023-05-22 14:11:12 +0300

    My first commit

--- app/src/main/java/com/example/medicslepyshev/MainActivity.java
+++ app/src/main/java/com/example/medicslepyshev/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+
+            }
+        }, 3000);
     }
 }
--- app/src/main/java/com/example/medicslepyshev/MainActivity2.java
+++ app/src/main/java/com/example/medicslepyshev/MainActivity2.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -12,11 +14,15 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity2.this,MainActivity3.class);
-            }
-        }, 3000);
+
+        LinearLayout screen = (LinearLayout) findViewById(R.id.lin);
+
     }
+
+    public void onClick(View v){
+        Intent intent = new Intent(MainActivity2.this,MainActivity3.class);
+        startActivity(intent);
+
+    }
+
 }
