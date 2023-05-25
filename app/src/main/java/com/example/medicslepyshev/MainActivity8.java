aa82ab28f80d2c113a2c4fdeef6710b97d64fc89
rmp <|"/>
2023-05-25 11:11:42 +0300

    My first commit

--- app/src/main/java/com/example/medicslepyshev/MainActivity8.java
+++ app/src/main/java/com/example/medicslepyshev/MainActivity8.java
@@ -2,7 +2,9 @@ package com.example.medicslepyshev;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity8 extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity8 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main8);
+    }
+    public void onClickHandler8(View view) {
+        Intent intent = new Intent(this, MainActivity9.class);
+        startActivity(intent);
     }
 }
